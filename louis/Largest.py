48b79558b207daf783ff67334b12fb3104f93fd3
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-13 13:24:50 +0800

    Largest number in the list

--- louis/Largest.py
+++ louis/Largest.py
@@ -1,4 +1,3 @@
-s = [45, 8, 15, 14, 23, 42]
-# call int(x) on each element before comparing it
-s.sort(key=int)
-print ('{d[5]}'.format(d=s))
+list1 = [45, 8, 15, 14, 23, 42]
+list1.sort()
+print("Largest element is:",list1[len(list1) - 1])
