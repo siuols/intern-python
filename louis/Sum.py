e55b165fdc9460de267f2d638fb6b4e133f2c438
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-13 13:38:30 +0800

    Total of all integers in the list

---
+++ louis/Sum.py
@@ -0,0 +1,4 @@
+list1 = [45, 8, 15, 14, 23, 42] # = 147
+# print("The total of all the integers in the list is:",list1[len(list1) - 1])
+Total = sum(list1)
+print("The total of all the integers in the list is: ",Total)
