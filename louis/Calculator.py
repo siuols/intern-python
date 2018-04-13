b5ddf442b3364397ffb561677841c54cdc979219
dagusen <louisaleksieb.dagusen@gmail.com>
2018-04-13 18:18:50 +0800

    added except ValueError

--- louis/Calculator.py
+++ louis/Calculator.py
@@ -1,38 +1,38 @@
 class Calculator:
-	def __init__(self, num1, num2):
-		self.num1 = num1
-		self.num2 = num2
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
 
-	def addition(self):
-		return int(self.num1 + self.num2)
+    def addition(self):
+        return int(self.num1 + self.num2)
 
-	def subtraction(self):
-		return int(self.num1 - self.num2)
+    def subtraction(self):
+        return int(self.num1 - self.num2)
 
-	def multiplication(self):
-		return int(self.num1 * self.num2)
+    def multiplication(self):
+        return int(self.num1 * self.num2)
 
-	def division(self):
-		return int(self.num1 / self.num2)
-
-#converted from string to integer
-num1 = input("Input first number: ")
-operator = input("Mathematical operator: ")
-num2 = input("Input second number: ")
+    def division(self):
+        return int(self.num1 / self.num2)
 
 try:
-    #pass input data to class
-	obj = Calculator(num1,num2)
+    #converted from string to integer
+    num1 = input("Input first number: ")
+    operator = input("Mathematical operator: ")
+    num2 = input("Input second number: ")
 
-	if operator == "+":
-		print ("The Sum {} and {} is: ".format(num1,num2),obj.addition())
-	elif operator == "-":
-		print ("The Diffirence {} and {} is: ".format(num1,num2),obj.subtraction())
-	elif operator == "*":
-		print ("The Product {} and {} is: ".format(num1,num2),obj.multiplication())
-	elif operator == "/":
-		print ("The Quotient {} and {} is: ".format(num1,num2),obj.division())
-	else:
-	    print("Invalid operator")
-except:
-    print ("Sorry you inputted incorrect data type")
+    #pass input data to class
+    obj = Calculator(num1,num2)
+
+    if operator == "+":
+        print ("The Sum {} and {} is: ".format(num1,num2),obj.addition())
+    elif operator == "-":
+        print ("The Diffirence {} and {} is: ".format(num1,num2),obj.subtraction())
+    elif operator == "*":
+        print ("The Product {} and {} is: ".format(num1,num2),obj.multiplication())
+    elif operator == "/":
+        print ("The Quotient {} and {} is: ".format(num1,num2),obj.division())
+    else:
+        print("Invalid operator")
+except ValueError:
+    print("Oops!  That was no valid number.  Try again...")
